cabc919e748f48b699edf72d46733835680d7a4f
Sparsh Bansal <115392431+bansalsparsh@users.noreply.github.com>
2025-05-17 09:24:39 +0530

    BaseTest.java

--- tests/BaseTest.java
+++ tests/BaseTest.java
@@ -6,7 +6,7 @@ import org.testng.annotations.BeforeClass;
 public class BaseTest {
     protected BookingService service;
 
-    @BeforeClass
+    @BeforeMethod
     public void setUp() {
         service = new BookingService();
     }
